2ece63c8a021a40d355d19d99d0f847747f88628
nmoun <39025757+nmoun@users.noreply.github.com>
2019-02-05 08:00:41 +0100

    Fix & use reduce

--- index.js
+++ index.js
@@ -2,33 +2,27 @@ const path = require('path')
 const fs = require('fs')
 const moment = require('moment')
 
-main()
 
-function main(){
-  const filename = process.argv[2]
-  const offset = parseInt(process.argv[3], 10)
+main(process.argv[2], parseInt(process.argv[3], 10))
 
+function main(filename, offset){
   console.log(`file: ${filename}`)
   console.log(`offset in ms: ${offset}`)
 
   const reg = new RegExp(/^([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}) --> ([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3})$/, 'g')
-  const content = fs.readFileSync(path.resolve(__dirname, 'srt', filename), 'utf8')
+  const content = fs.readFileSync(path.resolve(__dirname, filename), 'utf8')
   const arr = content.split('\r\n')
-  console.log('length arr: ' + arr.length )
 
-  let readLine = null,
-    result = ''
-
-  for(let i = 0 ; i < arr.length ; i++){
-    readLine = reg.exec(arr[i])
-    if(readLine){
-      let start = offsetTime(readLine[1], offset)
-      var end = offsetTime(readLine[2], offset)
-      result += start.format('HH:mm:ss,SSS') + ' --> ' + end.format('HH:mm:ss,SSS') + '\r\n'
-    } else {
-      result += arr[i] + '\r\n'
-    }
-  }
+  let result = arr.reduce((acc, el) => {
+      let readLine = reg.exec(el)
+      if(readLine){
+        let start = offsetTime(readLine[1], offset)
+        var end = offsetTime(readLine[2], offset)
+        return acc + start.format('HH:mm:ss,SSS') + ' --> ' + end.format('HH:mm:ss,SSS') + '\r\n'
+      } else {
+        return acc + el + '\r\n'
+      }
+    }, '')
   fs.writeFileSync(path.resolve(__dirname, 'results', filename.split('.srt')[0] + '_result.srt'), result)
 }
 
